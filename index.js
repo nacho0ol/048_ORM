556825a07db1581ee215d52aff03abfc6d85bd23
nachool <nachma.olivia1394@gmail.com>
2025-10-27 11:45:56 +0700

    setting port & express

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const db = require("./models");
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
